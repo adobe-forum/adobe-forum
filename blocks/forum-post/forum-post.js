bc873fad2e5ff8621a6dcd5e42ec843e45c0bf73
glorioustitan5 <vdivyeshan@adobe.com>
2026-02-18 10:50:42 +0530

    Normal arrow Icon changed to spectrum

--- blocks/forum-post/forum-post.js
+++ blocks/forum-post/forum-post.js
@@ -92,9 +92,8 @@ const ContentBlock = ({ block }) => {
 };
 
 const ArrowIcon = () => html`
-  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-    <line x1="5" y1="12" x2="19" y2="12"></line>
-    <polyline points="12 5 19 12 12 19"></polyline>
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 18 18">
+    <path d="M11.5 8.5H2v1h9.5l-3.5 3.5 .7.7 4.7-4.7-4.7-4.7-.7.7 3.5 3.5z" fill="currentColor"/>
   </svg>
 `;
 
